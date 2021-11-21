954a64e9fb9a3754b31bdbbbe3cfd787bc8cccbf
g-kelvin <kelvingauki@gmail.com>
2021-11-21 18:06:08 +0300

    first commit

--- message.php
+++ message.php
@@ -16,8 +16,8 @@
     <meta name="author" content="">
 
     <!-- Site Icons -->
-    <link rel="shortcut icon" href="images/use-01.png" type="image/x-icon">
-    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+  <link rel="shortcut icon" href="images/use-01.png" type="image/x-icon">
+    <link rel="apple-touch-icon" href="images/use-01.png">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -60,12 +60,7 @@
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                    <div class="login-box">
-                        <select id="basic" class="selectpicker show-tick form-control" data-placeholder="Sign In">
-                            <option>Register Here</option>
-                            <option>Sign In</option>
-                        </select>
-                    </div>
+                   
                     <div class="text-slid-box">
                         <div id="offer-box" class="carouselTicker">
                             <ul class="offer-box">
@@ -118,9 +113,9 @@
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="navbar-menu">
-                     <ul class="nav navbar-nav ml-auto" data-in="fadeInDown" data-out="fadeOutUp">
+                   <ul class="nav navbar-nav ml-auto" data-in="fadeInDown" data-out="fadeOutUp">
                         <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-                        <li class="nav-item active"><a class="nav-link" href="about.html">About Us</a></li>
+                        <li class="nav-item"><a class="nav-link" href="about.html">About Us</a></li>
                         <li class="dropdown">
                             <a href="#" class="nav-link dropdown-toggle arrow" data-toggle="dropdown">PRODUCTS</a>
                             <ul class="dropdown-menu" >
@@ -137,7 +132,19 @@
                             </ul>
                         </li>
                         <li class="nav-item"><a class="nav-link" href="gallery.html">Gallery</a></li>
-                        <li class="nav-item"><a class="nav-link" href="contact.html">Contact Us</a></li>
+                        <li class="nav-item active"><a class="nav-link" href="contact.html">Contact Us</a></li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+
+                <!-- Start Atribute Navigation -->
+                <div class="attr-nav">
+                    <ul>
+                        <li class="search"><a href="#"><i class="fa fa-search"></i></a></li>
+                        <li class="side-menu"><a href="#">
+                        <i class="fa fa-shopping-bag"></i>
+                           
+                    </a></li>
                     </ul>
                 </div>
                 <!-- End Atribute Navigation -->
@@ -192,10 +199,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h2>Contact Us</h2>
+                    <h2>Shop</h2>
                     <ul class="breadcrumb">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active"> Contact Us </li>
+                        <li class="breadcrumb-item active">Shop</li>
                     </ul>
                 </div>
             </div>
@@ -203,84 +210,56 @@
     </div>
     <!-- End All Title Box -->
 
-    <!-- Start Contact Us  -->
-    <div class="contact-box-main">
+    <!-- Start Shop Page  -->
+    <div class="shop-box-inner">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-sm-12">
-                    <div class="contact-form-right">
-                        <h2>GET IN TOUCH</h2>
-                        <form action="message.php" method="POST">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" name="name" placeholder="Your Name" required data-error="Please enter your name">
-                                        <div class="help-block with-errors"></div>
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <input type="text" placeholder="Your Email"  class="form-control" name="email" required data-error="Please enter your email">
-                                        <div class="help-block with-errors"></div>
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required data-error="Please enter your Subject">
-                                        <div class="help-block with-errors"></div>
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <textarea class="form-control" name="message" placeholder="Your Message" rows="4" data-error="Write your message" required></textarea>
-                                        <div class="help-block with-errors"></div>
-                                    </div>
-                                    <div class="submit-button text-center">
-                                        <button class="btn hvr-hover" name="submit" type="submit">Send Message</button>
-                                        <div id="msgSubmit" class="h3 text-center hidden"></div>
-                                        <div class="clearfix"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-				<div class="col-lg-4 col-sm-12">
-                    <div class="contact-info-left">
-                        <h2>CONTACT INFO</h2>
-                      
-                        <ul>
-                            <li>
-                                <p><i class="fas fa-map-marker-alt"></i>African Dream Toy<br>
-                                                                            P.O. Box 3184, <br>
-                                                                            Village Market,<br>
-                                                                            Nairobi Kenya 00621 </p>
-                            </li>
-                            <li>
-                                <p><i class="fas fa-phone-square"></i>Phone: <a href="tel:+254 114 915 609">+254 114 915 609</a></p>
-                            </li>
-                            <li>
-                                <p><i class="fas fa-envelope"></i>Email: <a href="mailto:contactinfo@gmail.com">africandreamtoys@yahoo.com</a></p>
-                            </li>
 
-                            <li style="color: black">
-                                <div class="footer-top-box">
-                            <h3 style="color: black">Business Time</h3>
-                            <ul class="list-time">
-                                <li style="color: black">Monday - Friday: 08.00am to 05.00pm</li> <li style="color: black" >Saturday: 10.00am to 08.00pm</li> <li style="color: black" >Sunday: <span style="color: black">Closed</span></li>
-                            </ul>
-                        </div>
-                                
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+
+            <?php
+              if(isset($_POST['submit'])){
+                    $name = $_POST['name'];
+                    $email = $_POST['email'];
+                    $subject = $_POST['subject'];
+                    $message = $_POST['message']; 
+
+                    $to = "info@africandreamtoys.co.ke";
+                    // $subject = 'Message from GLEE HYGIENE SOLUTIONS LTD'." , ";
+                    $msg ="Name: ".$name." , "."Email: ".$email." , "." Subject: ".$subject." wrote the following: ".$message;
+                    $headers ="From: ".$email;
+                     if(mail($to,$subject,$msg,$headers)){
+                        
+                      echo "<b>Message Sent. Thank You $name for your message.<b>";
+
+                      
+            }
+
+            else {
+                echo "yolo";
+                
+            }
+
+                   
+
+
+                }
+                else
+                {
+                    echo "try again";
+                }
+            ?>
+
+          
+
+
+
+
+
+
+
         </div>
     </div>
-    <!-- End Cart -->
+    <!-- End Shop Page -->
 
-  
  <!-- Start Footer  -->
     <footer>
         <div class="footer-main">
@@ -397,5 +376,3 @@
     <script src="js/contact-form-script.js"></script>
     <script src="js/custom.js"></script>
 </body>
-
-</html>
